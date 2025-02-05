6122e665c615f9503b461d9db6ed4ccf4b6deffe
Gregory <pastsar@gmail.com>
2025-02-05 18:32:43 +0200

    lifo scraper works for title and content

--- scrapers/lifo_scraper.py
+++ scrapers/lifo_scraper.py
@@ -1,10 +1,11 @@
+import os
 from datetime import datetime
 from bs4 import BeautifulSoup
 import urllib.request
 from urllib.parse import quote
+from dotenv import load_dotenv
 
 import pandas as pd
-
 
 # Make an http query from url and create soup to parse
 def create_soup_from_url (url):
@@ -22,29 +23,37 @@ def create_soup_from_url (url):
 
 def main():
 
-    print("test");
-
     # List with queries
-    desired_queries = ['efood']
 
+    # Load keywords to search
+    load_dotenv()
+    search_keywords = os.getenv('KEYWORDS')
+    desired_queries = search_keywords.split(',')
+
+    # Search scope
+    search_page_url = 'https://www.lifo.gr/search?keyword='
+    page = 0
+    end_page = 1
+
+    # Set storage
     link_results = []
     article_results = []
     title_results = []
 
-    page = 0
     # Scrape for links in website database given the queries
     for query in desired_queries:
-        # Scraπe n pages
-        for i in range(1):
+        # Scrape n pages
+        for i in range(end_page):
             page = page+i
-            
-            soup = create_soup_from_url(url='https://www.lifo.gr/search?keyword=' + quote(query) + '&page=' + str(page))
+
+            # Get search page items
+            soup = create_soup_from_url(url=search_page_url + quote(query) + '&page=' + str(page))
             
             # Extracting number of link_results
             search = soup.find_all('div', attrs={'class':'container p-0'})
             
             # Search for articles within given tag:
-            for s in  search:
+            for s in search:
                 articles = soup.find_all('article', attrs={'class': 'row no-gutters mb-4 mb-lg-6'})
                 
                 # Extract the link of each article:
@@ -55,16 +64,18 @@ def main():
 
     # Scrape inside individual search results
     for i in range(len(link_results)):
-        
+
+        # Get single article
         soup = create_soup_from_url(url=link_results[i])
 
-        # Get article body and individual paragraphs within
+        # Get article body
         try:
             article_body = soup.find('div', attrs={'class':'article__body'}).findAll('p', recursive=False)
         except:
             continue
 
         list_paragraphs = []
+        complete_article = ""
         for paragraph in article_body:
 
             list_paragraphs.append(paragraph.text)
@@ -74,26 +85,29 @@ def main():
         
         # Get article title
         try:
-            article_title = soup.find('div', attrs={'class':'article__header'}).find('h1', recursive=False)
+            article_title = soup.find('header', attrs={'class':'article__header'}).find('h1', recursive=False)
             title_results.append(article_title.text)
         except:
             article_title = 'N/A'
             title_results.append(article_title)
         
-    # Add articles and titles to pandas dataframe
+    # Add data from articles in pandas dataframe
     articles_list = {'Article': article_results, 'Title': title_results, 'Date Scraped': datetime.now()}
     articles_df = pd.DataFrame(data=articles_list)
     cols = ['Article', 'Title', 'Date Scraped']
     articles_df = articles_df[cols]
+
     # Check for duplicates in df:
     # articles_df = articles_df[articles_df.duplicated()]
+
     # Drop duplicates in df:
     articles_df = articles_df.drop_duplicates()
-    print(articles_df);
-    articles_df.to_csv(r'data/test_lifo_articles.txt', index=False, sep=' ', header=False)
+    print(articles_df)
 
+    # Save to CSV
+    articles_df.to_csv(r'data/lifo_articles.csv', index=False, sep=',', header=False)
 
-    # A couple of development sanity checks:
+    # Development sanity checks:
     print(link_results)
     print(len(link_results))
 
@@ -105,5 +119,7 @@ def main():
 
     print(articles_df)
 
+
+# Run
 if __name__ == '__main__':
     main()
